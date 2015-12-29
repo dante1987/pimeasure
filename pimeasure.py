15ae7856d7338e55ee05a83edccada9577fb4f52
Krzysztof Skarupa <krzysztof.skarupa@gmail.com>
2015-12-29 12:46:36 +0100

    Fix in port numbers

--- pimeasure.py
+++ pimeasure.py
@@ -183,6 +183,7 @@ class StatusDaemon(Daemon):
         self.communication_port_send = int(kwargs['communication_port_send'])
         del kwargs['communication_ip']
         del kwargs['communication_port_send']
+        del kwargs['communication_port_receive']
         super(StatusDaemon, self).__init__(**kwargs)
 
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
